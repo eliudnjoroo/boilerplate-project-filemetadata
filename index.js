132c6b8c68bfc0ab31ae21e4e56fb76519934587
Eliud <124616502+eliudnjoroo@users.noreply.github.com>
2025-05-02 21:02:57 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,8 +13,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(express.urlencoded({ extended : true }));
 
-
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({ 
+              name : req.file.originalname, 
+              type: req.file.mimetype, 
+              size: req.file.size 
+            });
+  console.log("file: "+ req.file );
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
